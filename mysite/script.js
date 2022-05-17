3fd4e18e55778682230d9a0f44b3c47e49091409
Sergei T <ter_serg@hotmail.com>
2022-05-17 15:42:10 +0600

    default

--- mysite/script.js
+++ mysite/script.js
@@ -1,7 +1,7 @@
-let page = document.querySelector('.page');
-page.classList.add('dark-theme');
-let themeButtom = document.querySelector('.theme-button');
+let page = document.querySelector(".page");
+page.classList.add("dark-theme");
+let themeButtom = document.querySelector(".theme-button");
 themeButtom.onclick = function () {
-    page.classList.toggle('light-theme');
-    page.classList.toggle('dark-theme');
-}
+  page.classList.toggle("light-theme");
+  page.classList.toggle("dark-theme");
+};
